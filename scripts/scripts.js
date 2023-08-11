c2386ddf0136809301c25e6a4ce1457edbcfb9d8
sina <sinaprvnd.webdev@gmail.com>
2023-08-11 23:47:49 +0330

    fetch api

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,17 +8,20 @@ const page3 = document.querySelector("#main-3");
 const startBtn = document.querySelector("#start-btn");
 const locationBtn = document.querySelector("#access-location-btn");
 const btnPrev = document.querySelectorAll(".btn-prev");
+const SearchCityForm = document.querySelector("#search-city-form");
+const SearchCityInput = document.querySelector("#search-city-input");
 
 //! VARIABLES
 let pageNum = 1;
 
+//! NAVIGATE BETWEEN PAGES
 function pageChange() {
   const currentPage = document.querySelector(`#main-${pageNum}`);
   pages.forEach((page) => page.classList.add("d-none"));
   currentPage.classList.remove("d-none");
 }
 
-startBtn.addEventListener("click", () => {
+startBtn.addEventListener("click", (e) => {
   pageNum++;
   pageChange();
 });
@@ -34,3 +37,21 @@ btnPrev.forEach((btn) =>
     pageChange();
   })
 );
+
+SearchCityForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //! SEARCH CITY
+  if (SearchCityInput.value !== "") {
+    requestApi(SearchCityInput.value);
+    pageNum++;
+    pageChange();
+  }
+});
+
+//! SEARCH CITY REQUEST API
+const apiKey = `c31fe415291450443037b3e14668e91b`;
+function requestApi(city) {
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  fetch(api).then((response) => console.log(response.json()));
+}
